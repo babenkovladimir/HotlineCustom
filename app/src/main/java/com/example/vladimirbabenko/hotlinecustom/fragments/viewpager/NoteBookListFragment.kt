1e32494bd65558cc674154c25e6103271dd455b6
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-04-09 20:01:27 +0300

    add divier item decoration;

--- app/src/main/java/com/example/vladimirbabenko/hotlinecustom/fragments/viewpager/NoteBookListFragment.kt
+++ app/src/main/java/com/example/vladimirbabenko/hotlinecustom/fragments/viewpager/NoteBookListFragment.kt
@@ -2,22 +2,27 @@ package com.example.vladimirbabenko.hotlinecustom.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import com.example.vladimirbabenko.hotlinecustom.R
 import com.example.vladimirbabenko.hotlinecustom.data.DataManager
 import com.example.vladimirbabenko.hotlinecustom.fragments.viewpager.NoteBookRecyclerViewAdapter
+import kotlinx.android.synthetic.main.fragment_notebook_list.rvNoteBookRecyclerView
 import kotlinx.android.synthetic.main.fragment_notebook_list.view.rvNoteBookRecyclerView
 
-class NoteBookListFragment():Fragment() {
+class NoteBookListFragment() : Fragment() {
 
-  lateinit var dataManager:DataManager
+  lateinit var dataManager: DataManager
 
   companion object {
-    val title:String = "NoteBooks"
-    get
+    val title: String = "NoteBooks"
+      get
+
     fun newInstance(bundle: Bundle?): NoteBookListFragment {
       val fragment = NoteBookListFragment()
       fragment.arguments = bundle
@@ -33,13 +38,17 @@ class NoteBookListFragment():Fragment() {
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?): View? {
 
+    //TODO How to describe???
     val view: View = inflater.inflate(R.layout.fragment_notebook_list, container, false)
-    view.rvNoteBookRecyclerView.layoutManager = LinearLayoutManager(container!!.context)
+    val layoutManager = LinearLayoutManager(container!!.context, LinearLayout.VERTICAL, false)
 
+    view.rvNoteBookRecyclerView.layoutManager = layoutManager
 
-    val adapter: NoteBookRecyclerViewAdapter = NoteBookRecyclerViewAdapter()
+    val adapter = NoteBookRecyclerViewAdapter()
     view.rvNoteBookRecyclerView.adapter = adapter
 
+    val dividerItemDecoration = DividerItemDecoration(view.rvNoteBookRecyclerView.context, LinearLayout.VERTICAL)
+    view.rvNoteBookRecyclerView.addItemDecoration(dividerItemDecoration)
     adapter.setNoteBooks(dataManager.fetchMocks())
     return view
   }
