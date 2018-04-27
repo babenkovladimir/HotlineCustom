9dfa1befe62d10d2a546aff3b3b18e6fa4dede45
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-04-27 23:20:38 +0300

    refacktoring;

--- app/src/main/java/com/example/vladimirbabenko/hotlinecustom/fragments/sign_in_fragment_mvp/SignInPresenter.kt
+++ app/src/main/java/com/example/vladimirbabenko/hotlinecustom/fragments/sign_in_fragment_mvp/SignInPresenter.kt
@@ -1,22 +1,15 @@
 package com.example.vladimirbabenko.hotlinecustom.fragments.sign_in_fragment_mvp
 
-import android.app.PendingIntent.getActivity
-import android.support.design.widget.Snackbar
 import android.text.TextUtils
 import android.util.Log
-import android.view.WindowManager
 import android.widget.Toast
-import com.example.vladimirbabenko.hotlinecustom.base.App
-import com.example.vladimirbabenko.hotlinecustom.base.App.Companion
 import com.example.vladimirbabenko.hotlinecustom.base.mvp.BasePresenter
 import com.example.vladimirbabenko.hotlinecustom.entity.User
 import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import java.lang.Exception
 
 class SignInPresenter() : BasePresenter<SignInFragment>() {
 
@@ -48,15 +41,9 @@ class SignInPresenter() : BasePresenter<SignInFragment>() {
           } else Toast.makeText(getView()!!.context, task.exception?.localizedMessage,
             Toast.LENGTH_LONG).show()
         }
-      }).addOnFailureListener {
-        object : OnFailureListener {
-          override fun onFailure(exception: Exception) {
-            Log.d("TAGSIGNIN", "Noton Failru - " + exception.cause)
-          }
-        }
-      }
+      })
   }
-
+  
   private fun saveUserInfoToPreferences(fireUser: FirebaseUser?): Boolean {
     with(fireUser!!) {
       val id = uid
