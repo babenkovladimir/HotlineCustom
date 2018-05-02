9b6c99b9719a81ef2f09532c8dbf8d6c9ed6d100
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-05-02 19:25:33 +0300

    add new Realm objects;

--- app/src/main/java/com/example/vladimirbabenko/hotlinecustom/entity/BascketItemRealm.kt
+++ app/src/main/java/com/example/vladimirbabenko/hotlinecustom/entity/BascketItemRealm.kt
@@ -5,11 +5,11 @@ import io.realm.annotations.PrimaryKey
 import io.realm.annotations.RealmClass
 import io.realm.annotations.Required
 
-//@RealmClass open class BascketItemRealm
-//
-//  constructor(
-//    @PrimaryKey var id: String = "",
-//    @Required var name : String= "",
-//    @Required var price : Int= 1,
-//              val photoUrl : String,
-//    @Required var num:Int = 1) : RealmObject()
+@RealmClass open class BascketItemRealm
+
+  constructor(
+    @PrimaryKey var id: String? = "",
+    @Required var name : String ? = "",
+    var price : Int?  = 1,
+    var photoUrl : String? = "",
+    var num:Int? = 1) : RealmObject()
--- app/src/main/java/com/example/vladimirbabenko/hotlinecustom/entity/VideoCardRealm.kt
+++ app/src/main/java/com/example/vladimirbabenko/hotlinecustom/entity/VideoCardRealm.kt
@@ -1,16 +1,17 @@
-//package com.example.vladimirbabenko.hotlinecustom.entity
-//
-//import io.realm.RealmModel
-//import io.realm.annotations.PrimaryKey
-//import io.realm.annotations.RealmClass
-//import io.realm.annotations.Required
-//
-//@RealmClass
-//open class VideoCardRealm constructor(
-//  @Required @PrimaryKey  var id: String? = "",
-//  var name: String? = "",
-//  var memorySize: Int? = null,
-//  var price: Int? = null,
-//  var photoUrl: String? = null,
-//  var description: String? = null
-//): RealmModel
+package com.example.vladimirbabenko.hotlinecustom.entity
+
+import io.realm.RealmModel
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmClass
+import io.realm.annotations.Required
+
+@RealmClass
+open class VideoCardRealm constructor(
+  @Required @PrimaryKey  var id: String? = "",
+  var name: String? = "",
+  var memorySize: Int? = null,
+  var price: Int? = null,
+  var photoUrl: String? = null,
+  var description: String? = null
+): RealmObject()
